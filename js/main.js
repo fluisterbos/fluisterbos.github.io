eb1fb9276427ec7d3ea7688df6ac8ee37a2a9f78
Marc Portier <marc.portier@gmail.com>
2014-03-15 19:09:21 +0100

    adding the backgrounds

--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
 (function ($){
-  var $tgl = $("#nav_menu_toggle");
-  var $hideToRight = $(".hide-right");
-  var $hideToTop =  $(".hide-top");
-  var $hideToFade =  $(".hide-fade");
-  var animopts = {duration: 400, queue: false};
-  if ($tgl) {
-    function hideElements () {
-      $tgl.rotate({animateTo:-90});
-      $hideToRight.animate({width: "toggle"},animopts);
-      $hideToTop.slideUp(animopts);
-      $hideToFade.fadeOut(animopts);
-    }
+  /* wait for the page to be loaded */
+  $(function(){
+    /* Enable the toggle effect 
+    --------------------------------------------------------- */
+    var $tgl = $("#nav_menu_toggle");
+    var $hideToRight = $(".hide-right");
+    var $hideToTop =  $(".hide-top");
+    var $hideToFade =  $(".hide-fade");
+    var animopts = {duration: 400, queue: false};
 
-    function showElements () {
-      $tgl.rotate({animateTo:0});
-      $hideToRight.animate({width: "toggle"},animopts);
-      $hideToTop.slideDown(animopts);
-      $hideToFade.fadeIn(animopts);
-    }
-
-    function alternate(fnOn, fnOff) {
-      var on = true;
-      return function() {
-        if (on) { fnOn(); } else { fnOff(); }
-        on = !on;
+    if ($tgl) { // if a tgl is found
+      function hideElements () {
+        $tgl.rotate({animateTo:-90});
+        $hideToRight.animate({width: "toggle"},animopts);
+        $hideToTop.slideUp(animopts);
+        $hideToFade.fadeOut(animopts);
       }
+
+      function showElements () {
+        $tgl.rotate({animateTo:0});
+        $hideToRight.animate({width: "toggle"},animopts);
+        $hideToTop.slideDown(animopts);
+        $hideToFade.fadeIn(animopts);
+      }
+
+      function alternate(fnOn, fnOff) {
+        var on = true;
+        return function() {
+          if (on) { fnOn(); } else { fnOff(); }
+          on = !on;
+        }
+      }
+
+      alternate.on = true;
+
+      $tgl.on('click', alternate(hideElements, showElements));
     }
 
-    alternate.on = true;
+
+    /* Enable the background-slide-show
+    --------------------------------------------------------- */
+    var $body = $('body');
+    var bgset = $body.data('bg');
+    if (bgset && bgset.length) { // if slides are defined
+      var slides = [];
+      for (var i=0; i<bgset.length; i++) {
+        slides[i] = {image: bgset[i]};
+      }
+      $.supersized({ slideshow: 1, vertical_center : 1, fit_portrait: 0, fit_landscape: 0, slides : slides });
+    }
 
 
-    $(function(){
-      $tgl.on('click', alternate(hideElements, showElements));
-    });
-  }
+  });
 })(jQuery);
--- nl/vakantiewoning/accommodatie.md
+++ nl/vakantiewoning/accommodatie.md
@@ -1,6 +1,7 @@
 ---
 layout: default
 title: Accomodatie
+bg: ost_sep
 ---
 
 TODO Text voor Accomodatie komt hier
