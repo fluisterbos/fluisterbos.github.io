b9b967c13ae652ecbd30792532b14e3e633324cc
Marc Portier <marc.portier@gmail.com>
2016-03-28 08:46:50 +0200

    exclude pdf from exclusion

--- dumptext.sh
+++ dumptext.sh
@@ -3,4 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 files="$(find ${DIR}/nl/ -type f| grep -v voorbeeld; echo "${DIR}/_data/navigation.yml")"
 
-for f in ${files}; do echo -e ">>>>>>>>\n$f\n>>>>>>>>\n\n"; cat $f; done
+for f in ${files}; do 
+  e="${f##*.}"
+  echo -e ">>>>>>>>\n$f\n[$e]\n>>>>>>>>\n\n"; 
+  if [ "$e" != "pdf" ]; then
+    cat $f; 
+  else
+    echo "-- TEXT NIET TUSSENGEVOEGD -- ";
+  fi
+done
