d45d23c2cf89ef3197919ba50b3bb7aa2abdf748
Marc Portier <marc.portier@gmail.com>
2018-04-08 08:27:01 +0200

    aanpassing generatie bestand voor vertaling

--- dumptext.sh
+++ dumptext.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-files="$(find ${DIR}/nl/ -type f| grep -v voorbeeld; echo "${DIR}/_data/navigation.yml")"
+files="$(find ${DIR}/nl/ -type f| grep -v voorbeeld; echo "${DIR}/_data/linkcode.yml"; echo "${DIR}/_data/menu.yml")"
 
 for f in ${files}; do 
   e="${f##*.}"
